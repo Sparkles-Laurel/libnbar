ec8764414626f2350f7d5534972dcb17d9b168be
Sparkles-Laurel <info@yigitovski.com>
2024-09-14 02:28:39 +0300

    add failure cleanup

--- src/libnbar_fhandle.c
+++ src/libnbar_fhandle.c
@@ -18,6 +18,7 @@ nbar_archive_t *nbar_fopen(char *filename, char *mode) {
     if((fp = fopen(filename, mode)) == NULL) {
         // bail out if file fails to open
         perror("nbar_fopen");
+        free(result);
         return NULL;
     } else {
         result->ar_file = fp;
@@ -28,12 +29,18 @@ nbar_archive_t *nbar_fopen(char *filename, char *mode) {
     // if the file lacks a proper header, bail out
     if(fread(&header, sizeof(nbar_archive_header_t), 1, result->ar_file) != sizeof(nbar_archive_header_t)) {
         perror("nbar_fopen: error while reading archive header");
+#       pragma region         
+            free(result);
+            fclose(fp);
+#       pragma endregion
         return NULL;
     } else if (header._magic_left != 0x0617 || header._magic_right != 0x1033) {
         // if the Page and Left magic numbers do not match, the archive
         // fails to pass the NBAR specification. Bail out.
         errno = EILSEQ;
         fprintf(stderr, "nbar_fopen: invalid archive format");
+        free(result);
+        fclose(fp);
         return NULL; 
     }
 
@@ -41,6 +48,8 @@ nbar_archive_t *nbar_fopen(char *filename, char *mode) {
     struct stat ar_stat;
     if(!fstat(fileno(fp), &ar_stat)) {
         perror("nbar_fopen: fstat");
+        free(result);
+        fclose(fp);
         return NULL;
     }
 
@@ -50,6 +59,10 @@ nbar_archive_t *nbar_fopen(char *filename, char *mode) {
         + header.file_length_1 + header.file_length_2)) {
             errno = EILSEQ;
             fprintf(stderr, "nbar_fopen: tampered archive header");
+
+            fclose(fp);
+            free(result);
+
             return NULL;
     }
 
@@ -59,12 +72,18 @@ nbar_archive_t *nbar_fopen(char *filename, char *mode) {
     // bail out if any of the files fail to create
     if(tmp_file_1 == NULL || tmp_file_2 == NULL) {
         perror("nbar_fopen: tmpfile");
+        fclose(fp);
+        free(result);
         return NULL;
     }
 
     // locate the archive file pointer to the first file
     if(fseek(fp, sizeof(nbar_archive_header_t), SEEK_SET)) {
         perror("nbar_fopen: fseek");
+        fclose(fp);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        free(result);
         return NULL;
     }
     
@@ -72,36 +91,70 @@ nbar_archive_t *nbar_fopen(char *filename, char *mode) {
     size_t *tmp_buffer_1;
     if((tmp_buffer_1 = calloc(header.file_length_1, 1)) == NULL) {
         perror("nbar_fopen: calloc");
+        fclose(fp);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        free(result);
         return NULL;
     }
 
     // load the first file.
     if(fread(tmp_buffer_1, header.file_length_1, 1, fp) != 1) {
         perror("nbar_fopen: fread");
+        free(tmp_buffer_1);
+        free(result);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        fclose(fp);
         return NULL;
     }
 
     // dump the first file into the temporary file
     if(fwrite(tmp_buffer_1, header.file_length_1, 1, tmp_file_1) != 1) {
         perror("nbar_fopen: fwrite");
+        free(tmp_buffer_1);
+        free(result);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        fclose(fp);
         return NULL;
     }
 
     size_t *tmp_buffer_2;
     if((tmp_buffer_2 = calloc(header.file_length_2, 1)) == NULL) {
         perror("nbar_fopen: calloc");
+        free(tmp_buffer_1);
+        free(result);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        fclose(fp);
         return NULL;
     }
 
     // load the second file
     if(fread(tmp_buffer_2, header.file_length_2, 1, fp) != 1) {
         perror("nbar_fopen: fread");
+        free(tmp_buffer_1);
+        free(tmp_buffer_2);
+        free(result);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        fclose(fp);
+   
         return NULL;
     }
 
     // dump the second file into the temporary file
     if(fwrite(tmp_buffer_2, header.file_length_2, 1, tmp_file_2) != 1) {
         perror("nbar_fopen: fwrite");
+
+        free(tmp_buffer_1);
+        free(tmp_buffer_2);
+        free(result);
+        fclose(tmp_file_1);
+        fclose(tmp_file_2);
+        fclose(fp);
+
         return NULL;
     }
 
