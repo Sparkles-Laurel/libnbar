6e42bfdb53ada308bdd80d0bf214d1720c5c6a8a
Sparkles-Laurel <info@yigitovski.com>
2024-09-14 21:58:29 +0300

    fix xmake

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ target("nbar")
 
 target("man-pages")
     set_kind("phony")
-    add_deps("libnbar")
+    add_deps("nbar")
     on_build(function (target)
         local man_pages = {"libnbar.3", "nbar_fopen.3", "nbar_fclose.3", "nbar_archive_t.3", "nbar_archive_header_t.3"}
         os.mkdir("doc/man")
