22b72ea03ac5fbbfaf011144727c9c90125c7d16
Sparkles-Laurel <info@yigitovski.com>
2024-09-14 21:52:31 +0300

    add man pages

--- xmake.lua
+++ xmake.lua
@@ -2,3 +2,14 @@ target("nbar")
     set_kind("shared")
     add_packages("sodium")
     add_files("src/*.c")
+
+target("man-pages")
+    set_kind("phony")
+    add_deps("libnbar")
+    on_build(function (target)
+        local man_pages = {"libnbar.3", "nbar_fopen.3", "nbar_fclose.3", "nbar_archive_t.3", "nbar_archive_header_t.3"}
+        os.mkdir("doc/man")
+        for _, page in ipairs(man_pages) do
+            os.copyfile("doc/" .. page, "doc/man/" .. page)
+        end
+    end)
